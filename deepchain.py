50a6e1f58d9235c745616f1613b0df140af10d24
tamiressantosdata <93625492+tamiressantosdata@users.noreply.github.com>
2022-03-10 21:29:35 -0300

    Add files via upload

---
+++ deepchain.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 21:24:32 2022
+
+@author: tamir
+"""
+
+from collections import ChainMap
+class DeepChainMap(ChainMap):
+    'Variant of ChainMap that allows direct updates to inner scopes'
+
+    def __setitem__(self, key, value):
+        for mapping in self.maps:
+            if key in mapping:
+                mapping[key] = value
+                return
+        self.maps[0][key] = value
+
+    def __delitem__(self, key):
+        for mapping in self.maps:
+            if key in mapping:
+                del mapping[key]
+                return
+        raise KeyError(key)
+
+d = DeepChainMap({'zebra': 'black'}, {'elephant': 'blue'}, {'lion': 'yellow'})
+d['lion'] = 'orange'         # update an existing key two levels down
+d['snake'] = 'red'           # new keys get added to the topmost dict
+del d['elephant']            # remove an existing key one level down
+print(d)                          # display result
